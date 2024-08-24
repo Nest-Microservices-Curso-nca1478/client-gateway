593e9258d1e3eaec375c70f9fdc551ffa18240db
Nelson Cadenas <nca1478@yahoo.com>
2024-08-23 23:56:02 -0400

    Levantar Products Microservice y conectarlo al Gateway

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductsController } from './products.controller';
+import { envs, PRODUCT_SERVICE } from '../config';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceHost,
+          port: envs.productsMicroservicePort,
+        },
+      },
+    ]),
+  ],
   controllers: [ProductsController],
   providers: [],
 })
