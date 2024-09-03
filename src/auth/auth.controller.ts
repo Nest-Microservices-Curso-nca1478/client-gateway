8d9083aee0734ebd4c7a801d59d355947508ca5f
Nelson Cadenas <nca1478@yahoo.com>
2024-09-02 20:31:10 -0400

    Login y Register DTOs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from '../config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser(@Body() registerUserDto: any) {
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.client.send('auth.register.user', registerUserDto);
   }
 
   @Post('login')
-  loginUser(@Body() loginUserDto: any) {
+  loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.client.send('auth.login.user', loginUserDto);
   }
 
