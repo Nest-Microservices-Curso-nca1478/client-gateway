1708fe06dc48fd513c3931d3ebc4a5f6ddef5cfe
Nelson Cadenas <nca1478@yahoo.com>
2024-09-07 20:06:33 -0400

    Health-check - Agregado controlador y módulo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
+import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
